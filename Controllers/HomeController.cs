d8e3af4710f49f20fd4c24f6541012493d1b3f9b
iparker3964 <iparker3964@yahoo.com>
2022-05-19 15:37:08 -0500

    Contact Form
    Implement contact form

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TheBlogProject.Models;
+using TheBlogProject.Services;
+using TheBlogProject.ViewModels;
+
+namespace TheBlogProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IBlogEmailSender _emailSender;
+        public HomeController(ILogger<HomeController> logger, IBlogEmailSender emailSender = null)
+        {
+            _logger = logger;
+            _emailSender = emailSender;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Contact(ContactMe model)
+        {
+            //Emailing will be taking place here
+            model.Message = $"{model.Message} <hr/> Phone: {model.Phone}";
+
+            await _emailSender.SendContactEmailAsync(model.Email, model.Name, model.Subject, model.Message);
+            return RedirectToAction("Index");
+
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        //Service is just an C# class that can be called upon to do certain units of work.
+    }
+}
---
+++ Services/DataService.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheBlogProject.Data;
+using TheBlogProject.Enums;
+using TheBlogProject.Models;
+
+namespace TheBlogProject.Services
+{
+    public class DataService
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<BlogUser> _userManager;
+        public DataService(ApplicationDbContext dbContext, RoleManager<IdentityRole> roleManager, UserManager<BlogUser> userManager)
+        {
+            _dbContext = dbContext;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public async Task ManageDataAsync()
+        {
+            //Create the DB fro the migrations
+            await _dbContext.Database.MigrateAsync();
+            //Task 1:Seed few Roles into the system.
+            await SeedRolesAsync();
+
+            //Task 2:Seed a few users into the system.
+            await SeedUsersAsync();
+        }
+
+        private async Task SeedRolesAsync()
+        {
+            //If there are roles in the system do nothing
+            if (_dbContext.Roles.Any())
+            {
+                return;
+            }
+            else
+            {
+                foreach (var role in Enum.GetNames(typeof(BlogRole)))
+                {
+                    //I need to use the role manager to create roles
+                    await _roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+        private async Task SeedUsersAsync()
+        {
+            //If there are users in the system do nothing
+            if (_dbContext.Users.Any())
+            {
+                return;
+            }
+            else
+            {
+                //Step 1: Creates a new instance of blog user
+                var adminUser = new BlogUser()
+                {
+                    Email = "iparker3964@gmail.com",
+                    UserName = "iparker3964@gmail.com",
+                    FirstName = "Isaiah",
+                    LastName = "Parker",
+                    PhoneNumber = "(800)555-1212",
+                    EmailConfirmed = true
+                    
+                };
+
+                //Step 2: Use the User manager to create a new user that is defined by adminUser
+                await _userManager.CreateAsync(adminUser, "Abc@123");
+
+                //Step 3: Add new user to administation role
+                await _userManager.AddToRoleAsync(adminUser, BlogRole.Admin.ToString());
+
+                //Step 1 Repeat: create the moderator user
+                var modUser = new BlogUser()
+                {
+                    Email = "cparker@theGreat.com",
+                    UserName = "cparker@theGreat.com",
+                    FirstName = "Chad",
+                    LastName = "Parker",
+                    PhoneNumber = "(800)555-1213",
+                    EmailConfirmed = true
+
+                };
+
+                //Step 2: Use the User manager to create a new user that is defined by modUser
+                await _userManager.CreateAsync(modUser, "Abc@123");
+
+                //Step 3: Add new user to moderator role
+                await _userManager.AddToRoleAsync(modUser, BlogRole.Moderator.ToString());
+            }
+        }
+    }
+}
